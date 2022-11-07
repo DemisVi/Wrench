a2ba3de543a5094f6e55621282cf3f5905b1e75c
d.liudkoi <114064097+DemisVi-work@users.noreply.github.com>
2022-11-07 12:05:20 +0300

    MVVM

---
+++ ViewModels/WorkerViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wrench.ViewModels
+{
+    internal class WorkerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        private int _counter;
+
+        public int Counter
+        {
+            get { return _counter % 100; }
+            set
+            {
+                _counter = value;
+                OnPropertyChanged(nameof(Counter));
+            }
+        }
+
+        public ICommand DoCommand
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    Counter++;
+                });
+            }
+        }
+    }
+}

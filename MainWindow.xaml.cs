5853d11e43a9b7468cfc76b5361eef7b7cafda9a
d.liudkoi <114064097+DemisVi-work@users.noreply.github.com>
2022-11-07 08:34:04 +0300

    Rename project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace work
+namespace Wrench
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

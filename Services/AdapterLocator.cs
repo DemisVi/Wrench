0de4a50372231b4b5d343135fa56a9dc02cc537d
d.liudkoi <114064097+DemisVi-work@users.noreply.github.com>
2022-11-15 16:40:36 +0300

    Write AdapterLocator

--- Services/AdapterLocator.cs
+++ Services/AdapterLocator.cs
@@ -6,26 +6,25 @@ using System.Threading.Tasks;
 
 using FTD2XX_NET;
 
-namespace Wrench.Services
+namespace Wrench.Service
 {
-    internal class AdapterLocator
+    public class AdapterLocator
     {
         private FTDI _ftdi = new FTDI();
         private uint _devCount = 0;
-        private FTDI.FT_DEVICE_INFO_NODE[] _ftdiDeviceList;
+        private FTDI.FT_DEVICE_INFO_NODE[] _ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[0];
 
-        public AdapterLocator()
+        public AdapterLocator() => Rescan();
+
+        public List<string> AdapterSerials { get { return _ftdiDeviceList.Select(x => x.SerialNumber).ToList(); } }
+
+        public uint AdapterCount => _devCount;
+
+        public void Rescan()
         {
             _ftdi.GetNumberOfDevices(ref _devCount);
             _ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[_devCount];
             _ftdi.GetDeviceList(_ftdiDeviceList);
         }
-
-        public List<string> AdapterSerials { get { return _ftdiDeviceList.Select(x => x.SerialNumber).ToList(); } }
-
-        //public List<string> AdapterSerials { get { return _ftdiDeviceList.Select(x => 
-        //    Encoding.UTF8.GetString(Encoding.Convert(Encoding.ASCII, Encoding.UTF8,
-        //    Encoding.ASCII.GetBytes(x.SerialNumber)))).ToList(); } }
-        public uint AdapterCount => _devCount;
     }
 }

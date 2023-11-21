b480c55e0a06ace563c44343ebff27be6bfae6f5
d.liudskoi <114064097+DemisVi-work@users.noreply.github.com>
2023-11-21 16:45:08 +0300

    Write Package Selector

---
+++ ViewModels/PackageSelectorViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using ReactiveUI;
+using Wrench.Models;
+using Wrench.Services;
+
+namespace Wrench.ViewModels;
+
+public class PackageSelectorViewModel : ViewModelBase
+{
+    private Firmware? selectedFirmware;
+    private ObservableCollection<Firmware>? firmwarePackages;
+
+    public PackageSelectorViewModel()
+    {
+        Refresh();
+    }
+    
+    public ObservableCollection<Firmware>? FirmwarePackages
+    {
+        get => firmwarePackages;
+        set => this.RaiseAndSetIfChanged(ref firmwarePackages, value);
+    }
+    public Firmware? SelectedFirmware
+    {
+        get => selectedFirmware;
+        set => this.RaiseAndSetIfChanged(ref selectedFirmware, value);
+    }
+    public Package? SelectedPackage { get; set; }
+
+    public void Refresh() => FirmwarePackages = new(FirmwareProvider.GetFirmware(Path.Combine(Environment.CurrentDirectory, "./SimCom_retro")));
+}

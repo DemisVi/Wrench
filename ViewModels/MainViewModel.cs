daee6b992ad7fc40cf8c2bd1206088398d08c7a3
d.liudskoi <114064097+DemisVi-work@users.noreply.github.com>
2023-12-07 19:40:14 +0300

    Work sync

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -4,9 +4,11 @@ using System.Reactive;
 using System.Reactive.Linq;
 using Avalonia.Media;
 using DynamicData;
+using Iot.Device.FtCommon;
 using ReactiveUI;
 using Wrench.Models;
 using Wrench.ViewModels;
+using Wrench.Views;
 
 namespace Wrench.ViewModels;
 
@@ -14,15 +16,16 @@ public class MainViewModel : ViewModelBase
 {
     private Package? package;
 
-    public ControlViewModel ControlViewModel { get; set; } = new();
-    public StatusViewModel StatusViewModel { get; set; } = new()
+    public MainViewModel()
     {
-        Bad = 20,
-        Good = 40000,
-        ContactUnit = "CanCom150",
-        Elapsed = TimeSpan.FromSeconds(500),
-        SerialNumber = "asdfasdfasdf1",
-        Label = "wtf",
-    };
+        var fts = Ftx232HDevice.GetFtx232H();
+        if (fts is { Count: > 0 })
+            StatusViewModel.ContactUnit = fts.First().SerialNumber.TrimEnd('A', 'B');
+        
+        
+    }
+
+    public ControlViewModel ControlViewModel { get; set; } = new();
+    public StatusViewModel StatusViewModel { get; set; } = new();
     public Package? Package { get => package; set => this.RaiseAndSetIfChanged(ref package, value); }
 }
--- ViewModels/StatusViewModel.cs
+++ ViewModels/StatusViewModel.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Threading;
 using Avalonia.Media;
+using Iot.Device.Nmea0183.Sentences;
 using ReactiveUI;
 using Wrench.Models;
 using Wrench.ViewModels;
+using Iot.Device.Rfid;
 
 namespace Wrench.ViewModels;
 
 public class StatusViewModel : ViewModelBase
 {
     private TimeSpan elapsed;
+    private Timer timeUpdater;
+    private DateTime? currentTime;
 
     public StatusViewModel()
     {
-        Task.Factory.StartNew(() => 
-        {
-            while (true)
-            {
-                Elapsed += TimeSpan.FromSeconds(1);
-                Task.Delay(1000).Wait();
-            }
-        });
+        timeUpdater = new((_) => CurrentTime = DateTime.Now, null, 0, 1000);
     }
     public string Label { get; set; } = "p-holder";
     public string ContactUnit { get; set; } = string.Empty;
     public string SerialNumber { get; set; } = string.Empty;
     public TimeSpan Elapsed { get => elapsed; set => this.RaiseAndSetIfChanged(ref elapsed, value); }
+    public DateTime? CurrentTime { get => currentTime; set => this.RaiseAndSetIfChanged(ref currentTime, value); }
     public int Good { get; set; }
     public int Bad { get; set; }
 }

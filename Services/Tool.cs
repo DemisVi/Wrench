0cb51bed00e9bd479f5b25f414b04ea2cd99e2e3
DemisVi <87558735+DemisVi@users.noreply.github.com>
2023-12-18 00:22:16 +0300

    refactor tool

--- Services/Tool.cs
+++ Services/Tool.cs
@@ -1,15 +1,17 @@
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 
 namespace Wrench.Services;
 
 public abstract class Tool
 {
+    private int errorExitCode = -1;
     public abstract string ToolPath { get; }
     public virtual string LastStdOut { get; protected set; } = string.Empty;
     public virtual string LastStdErr { get; protected set; } = string.Empty;
 
-    public virtual int Run(string command, int timeout = 2)
+    public virtual int Run(string command = "", int timeout = 2)
     {
         using var process = new Process()
         {
@@ -23,13 +25,26 @@ public abstract class Tool
             },
         };
 
-        process.Start();
-        process.WaitForExit(TimeSpan.FromSeconds(timeout));
+        try
+        {
+            process.Start();
+            process.WaitForExit(TimeSpan.FromSeconds(timeout));
 
-        if (!process.HasExited) process.Kill();
+            if (!process.HasExited) process.Kill();
 
-        LastStdErr = process.StandardError.ReadToEnd();
-        LastStdOut = process.StandardOutput.ReadToEnd();
+            LastStdErr = process.StandardError.ReadToEnd();
+            LastStdOut = process.StandardOutput.ReadToEnd();
+        }
+        catch (Win32Exception ex)
+        {
+            LastStdErr = ex.Message;
+            return errorExitCode;
+        }
+        catch (Exception ex)
+        {
+            LastStdErr = ex.Message;
+            return errorExitCode;
+        }
 
         return process.ExitCode;
     }
